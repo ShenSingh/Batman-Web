5ca3e3b4f02a4a7c63442260a0971b04c85ff88d
Shen8Singh <gayashan9090@gmail.com>
2023-09-27 05:27:19 +0530

    2023/09/27

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FightComponent } from './fight/fight.component';
+import { HomeComponent } from './home/home.component';
+import { LifeComponent } from './life/life.component';
+import { WallpaperComponent } from './wallpaper/wallpaper.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'fight',
+    component:FightComponent
+  },
+  {
+    path:'life',
+    component:LifeComponent
+  },
+  {
+    path:'wallpaper',
+    component:WallpaperComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LifeComponent } from './life/life.component';
+import { FightComponent } from './fight/fight.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { WallpaperComponent } from './wallpaper/wallpaper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LifeComponent,
+    FightComponent,
+    FooterComponent,
+    HeaderComponent,
+    WallpaperComponent
   ],
   imports: [
     BrowserModule,
