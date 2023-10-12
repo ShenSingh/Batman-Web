a90bb522572ca4e7165218d07517e12e7088ac10
Shen8Singh <gayashan9090@gmail.com>
2023-10-12 11:33:00 +0530

    fit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { FightComponent } from './fight/fight.component';
 import { HomeComponent } from './home/home.component';
 import { LifeComponent } from './life/life.component';
-import { WallpaperComponent } from './wallpaper/wallpaper.component';
-
-const routes: Routes = [
+import { WallpaperComponent } from './wallpaper/wallpaper.component'; 
+const routes: Routes =[
   {
     path:'',
     component:HomeComponent
